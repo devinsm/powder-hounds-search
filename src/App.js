bb73b3bda59c0d87cf38da42daf2bea214fda746
Devin McAllester <devinmcallester@gmail.com>
2018-08-20 17:48:59 -0500

    Adds basic header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import Banner from './Banner/Banner';
+import Header from './Header/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Banner />
+      <Header />
     );
   }
 }
