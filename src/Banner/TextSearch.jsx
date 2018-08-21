2cd926baa5c16310a5901096ed3809882c4fcef8
Devin McAllester <devinmcallester@gmail.com>
2018-08-21 11:00:09 -0500

    Adds styling to Banner

--- src/Banner/TextSearch.jsx
+++ src/Banner/TextSearch.jsx
@@ -6,19 +6,21 @@ import './TextSearch.css';
 export default class TextSearch extends Component {
   render() {
     return (
-      <form>
+      <form className="TextSearch">
         <label htmlFor="propertySearchBox">
           What are you looking for?
         </label>
-        <input
-          type="text"
-          placeholder="Search..."
-          className="form-control"
-          id="propertySearchBox"
-        />
-        <button type="submit" className="btn btn-success">
-          Search
-        </button>
+        <div id="searchInputGroup">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="form-control"
+            id="propertySearchBox"
+          />
+          <button type="submit" className="btn btn-success">
+            Search
+          </button>
+        </div>
       </form>
     );
   }
